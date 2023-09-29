992cab230f96f1c26ece1a0e7a30f1894f0cd76a
rawera_daniel <rawera.daniel@gmail.com>
2023-09-29 17:27:05 +0300

    style: apply styling to components for an enhanced user UI experience

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -6,7 +6,7 @@ function Menu() {
   const menu = useLoaderData();
 
   return (
-    <ul>
+    <ul className=" divide-stone-200 px-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
